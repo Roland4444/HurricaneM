40a83016131cd0812bff9bab968715d6cf0c7f70
Roland4444 <turbopollux@rambler.ru>
2018-10-17 09:37:09 +0000

    Hurricane ready. Maven Build

--- src/main/scala/hurricane.scala
+++ src/main/scala/hurricane.scala
@@ -1,6 +1,13 @@
+import java.time.{Duration, Period}
+import java.time.temporal.TemporalAmount
+import java.{lang, util}
+import java.util.Map
+import java.util.concurrent.TimeUnit
+
 import akka.actor.Actor
 import akka.actor.ActorSystem
 import akka.actor.Props
+import com.typesafe.config._
 
 class FirstActor extends Actor {
   def receive = {
@@ -17,7 +24,44 @@ class SecondActor extends Actor {
 }
 
 object Main extends App {
-  val system = ActorSystem("hurricane")
+
+  val conf = """
+  akka {
+   actor {
+     provider = remote
+   }
+   remote {
+     artery {
+       enabled = on
+       transport = aeron-udp
+       canonical.hostname = "127.0.0.1"
+       canonical.port = 25555
+     }
+   }
+ }
+"""
+
+ // ConfigFactory.parseString(conf)
+ // ConfigFactory.load()
+
+  import com.typesafe.config.ConfigFactory
+  val config: Config = ConfigFactory.parseString(conf)
+
+
+
+  import com.typesafe.config.ConfigFactory
+
+  val regularConfig = ConfigFactory.load
+  // override regular stack with myConfig
+  val combined = config.withFallback(regularConfig)
+
+  ConfigFactory.load
+
+
+  val system = ActorSystem("hurricane", config)
+
+
+
   //system.logConfiguration()
   // default Actor constructor
   val FirstActor = system.actorOf(Props[FirstActor], name = "firstactor")
