7b5154b29b5b5201c7261f3944ee70597414cec3
Roland4444 <turbopollux@rambler.ru>
2018-10-17 11:30:01 +0000

    Load sets fia file

--- src/main/scala/hurricane.scala
+++ src/main/scala/hurricane.scala
@@ -1,3 +1,6 @@
+import java.io.File
+import java.nio.file.{Files, Path, Paths}
+
 import akka.actor.Actor
 import akka.actor.ActorSystem
 import akka.actor.Props
@@ -14,23 +17,9 @@ class SecondActor extends Actor {
   }
 }
 object Main extends App {
-  val conf = """
-  akka {
-   actor {
-     provider = remote
-   }
-   remote {
-     artery {
-       enabled = on
-       transport = aeron-udp
-       canonical.hostname = "127.0.0.1"
-       canonical.port = 25555
-     }
-   }
- }
-"""
   import com.typesafe.config.ConfigFactory
-  val config: Config = ConfigFactory.parseString(conf)
+  val setts = new String(Files.readAllBytes(new File("./config.conf").toPath))
+  val config: Config = ConfigFactory.parseString(setts)
   val system = ActorSystem("hurricane", config)
   val FirstActor = system.actorOf(Props[FirstActor], name = "firstactor")
   val SecondActor = system.actorOf(Props[SecondActor], name = "secondactor")
