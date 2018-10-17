112c7fb0945289ba2689274a7c1d606052b67466
Roland4444 <turbopollux@rambler.ru>
2018-10-17 09:39:40 +0000

    load config via string

--- src/main/scala/hurricane.scala
+++ src/main/scala/hurricane.scala
@@ -1,30 +1,19 @@
-import java.time.{Duration, Period}
-import java.time.temporal.TemporalAmount
-import java.{lang, util}
-import java.util.Map
-import java.util.concurrent.TimeUnit
-
 import akka.actor.Actor
 import akka.actor.ActorSystem
 import akka.actor.Props
 import com.typesafe.config._
-
 class FirstActor extends Actor {
   def receive = {
-
     case x:String       => {println(x+"==>Just received     "+self.path)
                     sender!"received"}
   }
 }
-
 class SecondActor extends Actor {
   def receive = {
     case f      => println(self + "==>Just received   "+ f)
   }
 }
-
 object Main extends App {
-
   val conf = """
   akka {
    actor {
@@ -40,36 +29,10 @@ object Main extends App {
    }
  }
 """
-
- // ConfigFactory.parseString(conf)
- // ConfigFactory.load()
-
   import com.typesafe.config.ConfigFactory
   val config: Config = ConfigFactory.parseString(conf)
-
-
-
-  import com.typesafe.config.ConfigFactory
-
-  val regularConfig = ConfigFactory.load
-  // override regular stack with myConfig
-  val combined = config.withFallback(regularConfig)
-
-  ConfigFactory.load
-
-
   val system = ActorSystem("hurricane", config)
-
-
-
-  //system.logConfiguration()
-  // default Actor constructor
   val FirstActor = system.actorOf(Props[FirstActor], name = "firstactor")
   val SecondActor = system.actorOf(Props[SecondActor], name = "secondactor")
-
   FirstActor.tell("mx", SecondActor)
-
-
-
-
 }
